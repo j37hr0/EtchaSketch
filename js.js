e943ed5c83b87d7d6be161ed5451478147f95a1d
j37hr0 <jethro.cotton3@gmail.com>
2022-09-07 19:09:05 +0200

    re-did some functionality

--- js.js
+++ js.js
@@ -1,47 +1,45 @@
 const container = document.getElementById("container");
-var rows = document.getElementsByClassName("gridRow")
-var cells = document.getElementsByClassName("cell");
 const reset = document.getElementsByClassName("reset")
-defaultGrid()
 
+createGrid(16)
 
 function resetButton() 
  {
     container.replaceChildren()
     let gridSize = Number(prompt("Enter a grid size between 0 and 100: "))
+    if (gridSize >= 1 && gridSize <= 100){
         createGrid(gridSize)
     }
+    else {
+        createGrid(16)
+    }
+    }
 
-function mouseOver() {
+function mouseOver(color) {
     let hoverCell = this;
-    hoverCell.setAttribute("style", "background-color:black")
+    hoverCell.setAttribute("style", `background-color:${color}`)
 }
 
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
-
-function createGrid(size) {
-    makeRows(size);
-    makeColumns(size);
-}
-
-
-function makeRows(rowNum) {
-    for (i = 0; i < rowNum; i++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow"
+function createGrid(gridSize) {
+    container.style.setProperty('grid-template-rows', `repeat(${gridSize}, ${(1 / gridSize) * 600}px)`);
+    container.style.setProperty('grid-template-columns', `repeat(${gridSize}, ${(1 / gridSize) * 650}px)`);
+    for (let i = 0; i < gridSize * gridSize; i++)
+    { 
+        const child = document.createElement('div');
+        child.style.border = "1px solid teal";
+        container.appendChild(child);
+        child.style.backgroundColor = "white";
+        let randomColor = Math.floor(Math. random()*16777215).toString(16);
+        child.addEventListener('mouseover', () => {
+            if (document.querySelector('select').value === "Rainbow") {
+                child.style.backgroundColor = randomColor;
+            }
+            else if (document.querySelector('select').value === "Pink") {
+                child.style.backgroundColor = "pink"
+            }
+            else if (document.querySelector('select').value === "Black")
+            child.style.backgroundColor = "black"
+        });
+        
     }
 }
-
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++){
-        let newCell = document.createElement("div");
-        rows[j].appendChild(newCell).className = "cell";
-        newCell.addEventListener("mouseover", mouseOver, false)
-        }
-    }
-}
-
