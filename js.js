9cf2b3a5e559c4ee23bfe8a3e226c2362ac8d828
j37hr0 <jethro.cotton3@gmail.com>
2022-09-07 10:19:53 +0200

    basic functionality, grid laid out, user can reset button

---
+++ js.js
@@ -0,0 +1,48 @@
+const container = document.getElementById("container");
+var rows = document.getElementsByClassName("gridRow")
+var cells = document.getElementsByClassName("cell");
+const reset = document.getElementsByClassName("reset")
+defaultGrid()
+
+
+function resetButton() 
+ {
+    gridSize = Number(prompt("Enter a grid size: "))
+    createGrid(gridSize)
+
+}
+
+
+function mouseOver() {
+    let hoverCell = this;
+    hoverCell.setAttribute("style", "background-color:blue")
+}
+
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+function createGrid(size) {
+    makeRows(size);
+    makeColumns(size);
+}
+
+
+function makeRows(rowNum) {
+    for (i = 0; i < rowNum; i++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow"
+    }
+}
+
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++){
+        let newCell = document.createElement("div");
+        rows[j].appendChild(newCell).className = "cell";
+        newCell.addEventListener("mouseover", mouseOver, false)
+        }
+    }
+}
+
